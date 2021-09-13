71d18ecb0920d9041d2d6bb8289e7ff0fe4e2b6c
kobayashi0123 <xiaolinzhongzhi95@gmail.com>
2021-09-13 14:18:22 +0900

    explain

---
+++ php_ramen/sample.php
@@ -0,0 +1,15 @@
+<!DOCTYPE html>
+<html>
+<head>
+ <meta charset="utf-8"/>
+ <title>無題ドキュメント</title>
+</head>
+<body>
+<p>
+	<input type = "text" name = "your_name" id = "your_name"  size = "35" maxlength = "255"/>
+</p>
+<p>
+	<input type = "text" name = "e_mail" id = "e_mail" value = "Eメールアドレス" size = "50" maxlength = "500"/>
+<p>
+</body>
+</html>
